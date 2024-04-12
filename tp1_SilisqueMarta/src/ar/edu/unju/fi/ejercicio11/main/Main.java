f957d4786300a2183e8875d30ac2763355f00fcb
gdmar <silisquemarta@gmail.com>
2024-04-12 16:25:41 -0300

    desarrollo del ejercicio n11

---
+++ tp1_SilisqueMarta/src/ar/edu/unju/fi/ejercicio11/main/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio11.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i =2,j=40; j>0;i++,j--) {
+			int variable = i*j;
+			System.out.print(variable+" ");
+		}
+
+	}
+
+}
