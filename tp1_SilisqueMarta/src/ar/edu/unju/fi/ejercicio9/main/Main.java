73e8d26dbf5a694011bf8ade9e2f6ffc7f770303
gdmar <silisquemarta@gmail.com>
2024-04-11 21:37:56 -0300

    desarrollo del ejercicio n9

--- tp1_SilisqueMarta/src/ar/edu/unju/fi/ejercicio9/main/Main.java
+++ tp1_SilisqueMarta/src/ar/edu/unju/fi/ejercicio9/main/Main.java
@@ -1,9 +1,46 @@
 package ar.edu.unju.fi.ejercicio9.main;
 
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio9.model.Producto;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		Producto producto = new Producto();
+	
+		int variable =1;
+		do {
+			System.out.print("ingrese nombre del  producto;  ");	
+			String nombrep= scan.nextLine();
+			System.out.print("ingrese codigo del  producto;  ");	
+			String codigo= scan.nextLine();
+			System.out.print("ingrese precio del  producto;  ");	
+			float precio = scan.nextFloat();
+		
+			
+			producto.setNombre(nombrep);
+			producto.setCodigo(codigo);
+			producto.setPrecio(precio);
+			
+		
+				while(producto.getDescuento() == 0) {
+					System.out.print("ingrese  descuento:  ");	
+					int descuento = scan.nextInt();
+					producto.setDescuento(descuento);
+					
+				}
+				
+				scan.nextLine();
+			variable++;
+			System.out.println(producto.toString());
+			System.out.println("PRECIO CON DESCUENTO: "+producto.calcularDescuento());
+			
+		}while (variable <=3  );
+		
+		
 
 	}
 
--- tp1_SilisqueMarta/src/ar/edu/unju/fi/ejercicio9/model/Producto.java
+++ tp1_SilisqueMarta/src/ar/edu/unju/fi/ejercicio9/model/Producto.java
@@ -4,12 +4,16 @@ public class Producto {
 	private String nombre;
 	private String codigo;
 	private float  precio;
-	private float descuento;
+	private int descuento;
 	
 	
-	//public float calcularDescuento{
+	public float  calcularDescuento(){
+		float descuento2;
+		descuento2 =( this.descuento *this.precio) /100;
+		float resultado = this.precio - descuento2;
 		
-	//}
+		return resultado ;
+	}
 	
 	
 	public Producto() {
@@ -35,11 +39,26 @@ public class Producto {
 	public void setPrecio(float precio) {
 		this.precio = precio;
 	}
-	public float getDescuento() {
+	public int getDescuento() {
 		return descuento;
 	}
-	public void setDescuento(float descuento) {
+	
+	public void setDescuento(int descuento) {
 		this.descuento = descuento;
+		
+		if(descuento >=0 && descuento <=50) {
+			this.descuento=descuento;
+		}else {
+			System.out.println("ERROR el descuento no pertenece al rango");
+				
+		}
+	}
+
+
+	@Override
+	public String toString() {
+		return "Producto [nombre=" + nombre + ", codigo=" + codigo + ", precio=" + precio + ", descuento=" + descuento
+				+ "]";
 	}
 	
 	
